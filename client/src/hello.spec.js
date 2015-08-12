33db15fb822829d0b4f6716f3cb7d769f60531aa
Ryan Ricard <ryan.ricard@target.com>
2015-08-12 11:33:23 -0500

    My initial solution.

--- client/src/hello.spec.js
+++ client/src/hello.spec.js
@@ -1,1 +1,20 @@
+describe('Hello World:', function() {
+  var Hello,
+    element,
+    React,
+    TestUtils;
 
+  beforeEach(function () {
+    React = require('react/addons');
+    TestUtils = React.addons.TestUtils;
+  });
+
+  beforeEach(function () {
+    Hello = require('./hello');
+    element = TestUtils.renderIntoDocument(<Hello />);
+  });
+
+  it('should instantiate the Hello World', function () {
+    expect(TestUtils.isCompositeComponent(element)).to.be.true;
+  });
+});
---
+++ client/src/main.jsx
@@ -0,0 +1,4 @@
+var React = require('react/addons');
+var Hello = require('./hello');
+
+React.render(<Hello friend="YOLO!"/>, document.getElementById('app'));
